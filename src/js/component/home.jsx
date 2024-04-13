d2b6f74f0695922edb6a4c50064f2f653888a5d3
RikrdoLeal <140850677+RikrdoLeal@users.noreply.github.com>
2024-04-13 10:12:58 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,54 @@
+import propTypes from "prop-types";
 import React from "react";
+import ReactDOM from "react-dom";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+function SimpleCounter(props) {
+  return (
+    <div className=" bigCounter">
+      <div className="calendar">
+        <FontAwesomeIcon icon={faClock} style={{color: "#ffffff",}} />
+      </div>
+      <div className="four">
+        {props.digitFour % 10}
+      </div>
+      <div className="three">
+        {props.digitThree % 10}
+      </div>
+      <div className="two">
+        {props.digitTwo % 10}
+      </div>
+      <div className="one">
+        {props.digitOne % 10}
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+
+SimpleCounter.propTypes = {
+  digitFour: propTypes.number,
+  digitThree: propTypes.number,
+  digitTwo: propTypes.number,
+  digitOne: propTypes.number,
+};
+
+let counter = 0;
+
+setInterval(function () {
+  const four = Math.floor(counter / 1000);
+  const three = Math.floor(counter / 100);
+  const two = Math.floor(counter / 10);
+  const one = Math.floor(counter / 1);
+  counter++;
+
+  ReactDOM.render(
+    <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />,
+    document.querySelector("#app")
+  );
+}, 1000);
+
+export default SimpleCounter;
